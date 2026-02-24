dbb42eaa7917c8f9da6b7dffa3d7c2ae7e87a8ea
Kamila Faria <red.faria32@gmail.com>
2026-02-24 17:53:20 -0300

    feat: setup morgan and prom-client

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,14 +3,25 @@ import express from 'express';
 import { connectDatabase } from '../infrastructure/database/mongoose';
 import { connectRedis } from '../infrastructure/cache/redis';
 import { rabbitmq } from '../infrastructure/messaging/rabbitmq';
+import morgan from 'morgan';
+import * as promClient from 'prom-client';
+
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({ register: promClient.register });
 
 const app = express();
 app.use(express.json());
+app.use(morgan('combined'));
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK', service: 'Catalogo Microservice CQRS' });
+});
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', promClient.register.contentType);
+    res.end(await promClient.register.metrics());
 });
 
 async function startServer() {
